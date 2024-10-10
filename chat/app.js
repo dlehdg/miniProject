af3da94a1542052b3f66ed48f7fb6a8c40050337
dlehdg <ldg2409@naver.com>
2024-10-10 18:10:39 +0900

    env 파일 제거

--- chat/app.js
+++ chat/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config();
+require("dotenv").config();
 const cors = require("cors");
 const app = express();
 app.use(cors());
 
-
-mongoose.connect(process.env.DB).then(()=> console.log("connected to database"));
-
+mongoose
+  .connect(process.env.DB)
+  .then(() => console.log("connected to database"))
+  .catch((err) => console.log("현재 에러", err));
+ 
 module.exports = app;
-
-
