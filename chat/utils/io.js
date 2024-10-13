3914182d4e3ae320fdc42f3ff4b4628be81fffc9
dlehdg <ldg2409@naver.com>
2024-10-13 21:33:37 +0900

    유저 정보 가져오기, 정보 데이터베이스에 저장하기

--- chat/utils/io.js
+++ chat/utils/io.js
@@ -1,13 +1,22 @@
+let userController = require("../Controller/user.controller");
+
 module.exports = function (io) {
   // io.emit() -> 내보내는 기능
   // io.on() -> 들어주는 기능
-  io.on("connection", async(socket) => {
+  io.on("connection", async (socket) => {
     console.log("client is connected ", socket.id);
+
+    socket.on("login", async (username, cb) => {
+      try {
+        const user = await userController.saveUser(username, socket.id);
+        cb({ ok: true, data: user });
+      } catch (e) {
+        cb({ ok: false, error: e.message });
+      }
+    });
 
     socket.on("disconnect", () => {
       console.log("user 연결 종료");
-    })
+    });
   });
-
-  
-}
+};
